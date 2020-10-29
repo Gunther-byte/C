2f238bbafee1f3ce3d5221f0d87c078a22fe3925
Dannas Cornell <dannas.cornell@gmail.com>
2020-10-29 12:37:00 -0600

    Update scanf.c
    Se actualizó fórmula

--- variables/6_read/scanf.c
+++ variables/6_read/scanf.c
@@ -9,7 +9,7 @@ float l, a;
 printf("Introduce radio (entero): ");
 scanf("%d", &r);
 l=(2*PI)*r; 
-a=PI * pow (r,PI);
+a = PI * pow(r,2);
 printf("La longitud de la circunferencia vale %.2f\n", l);
 printf("El área del círculo vale %.2f\n", a);
 }
